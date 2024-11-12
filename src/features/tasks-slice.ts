b8e0c4bc86545b0fffac08bbb34424dfc04b9d74
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-11-12 20:53:26 +0300

    added actions

---
+++ src/features/tasks-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export type TasksState = {
+  entities: Task[];
+};
+
+const initialState: TasksState = {
+  entities: [],
+};
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<Task>) => {
+      state.entities.unshift(action.payload);
+    },
+    removeTask: (state, action: PayloadAction<Task>) => {
+        const indexOfTaskToRemove = state.entities.indexOf(action.payload);
+        state.entities.splice(indexOfTaskToRemove, 1);
+    },
+  },
+});
