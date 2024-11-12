3d5ebc9d1dea1bf27f2f5c551fc8d9c0090398ea
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-11-12 19:51:13 +0300

    added tasks slice

---
+++ src/features/tasks-slice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type TasksState = {
+    entities: Task[];
+
+}
+
+const initialState:TasksState = {
+    entities: []
+}
+
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {
+        addTask:(state) => state,
+        removeTask: (state) => state,
+    }
+})
